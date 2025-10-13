53822137c21aa9d377223a850b446b425a77fd37
Iceberg <gzs_iceberg@outlook.com>
2025-10-13 11:02:52 +0800

    Implement backward pass for NaiveAttention and FlashAttention, add tests for forward and backward correctness

--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -20,18 +20,47 @@ class NaiveAttention(torch.autograd.Function):
         batch_size, context_len, d_model = Q.shape
         sqrt_d = np.sqrt(d_model)
         scores = einsum(Q, K, "b i d, b j d -> b i j") / sqrt_d  # (B, S, S)
+        if is_casual:
+            mask = torch.tril(torch.ones((context_len, context_len), device=Q.device)).unsqueeze(0)  # (1, S, S)
+            mask = mask.to(torch.bool)
+            scores = torch.where(mask, scores, float("-inf"))
         row_max = torch.amax(scores, dim=-1, keepdim=True)  # (B, S, 1)
         p = torch.exp(scores - row_max)  # (B, S, S)
         row_sum = torch.sum(p, dim=-1, keepdim=True)  # (B, S, 1)
         p = p / row_sum  # (B, S, S)
         o = einsum(p, V, "b i j, b j d -> b i d")  # (B, S, D)
+        ctx.save_for_backward(Q, K, V, o, p)
+        ctx.is_casual = is_casual
+        ctx.q_shape = q_shape
         return o.reshape(q_shape)
 
     @staticmethod
     def backward(ctx, dO: Tensor) -> Tuple[Tensor, Tensor, Tensor, None]:
-        raise NotImplementedError(
-            "FlashAttention backward pass is not implemented yet."
-        )
+        Q, K, V, O, P = ctx.saved_tensors
+        is_casual = ctx.is_casual
+        q_shape = ctx.q_shape
+        dO = rearrange(dO, "... s d -> (...) s d")
+        batch_size, context_len, d_model = Q.shape
+
+        sqrt_d = np.sqrt(d_model)
+        dV = P.transpose(-2, -1) @ dO  # (B, D, query) @ (B, query, D) -> (B, D, D)
+        assert dV.shape == (batch_size, context_len, d_model)
+        dP = dO @ V.transpose(-2, -1)  # (B, query, key)
+        assert dP.shape == (batch_size, context_len, context_len)
+
+        dS = P * (dP  - torch.sum(dP * P, dim=-1, keepdim=True))  # (B, query, key)
+        if is_casual:
+            mask = torch.tril(torch.ones((context_len, context_len), device=Q.device)).unsqueeze(0)  # (1, S, S)
+            mask = mask.to(torch.bool)
+            dS = torch.where(mask, dS, 0.0)
+        dQ = dS @ K / sqrt_d  # (B, query, D)
+        assert dQ.shape == (batch_size, context_len, d_model)
+        dK = dS.transpose(-2, -1) @ Q / sqrt_d  # (B, key, D)
+        assert dK.shape == (batch_size, context_len, d_model)
+
+        return dQ.reshape(q_shape), dK.reshape(q_shape), dV.reshape(q_shape), None
+
+
 
 
 class FlashAttention(torch.autograd.Function):
@@ -122,9 +151,20 @@ class FlashAttention(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, dO: Tensor) -> Tuple[Tensor, Tensor, Tensor, None]:
-        raise NotImplementedError(
-            "FlashAttention backward pass is not implemented yet."
-        )
+        Q, K, V, O, L = ctx.saved_tensors
+        is_casual = ctx.is_casual
+        block_rows = ctx.block_rows
+        block_cols = ctx.block_cols
+        q_shape = ctx.q_shape
+
+        dQ, dK, dV = FlashAttention._backward(Q, K, V, O, L, is_casual, block_rows, block_cols, dO)
+        return dQ.reshape(q_shape), dK.reshape(q_shape), dV.reshape(q_shape), None
+
+    
+    @staticmethod
+    def _backward(Q: Tensor, K: Tensor, V: Tensor, O: Tensor, L: Tensor, is_casual: bool, block_rows: int, block_cols: int, dO: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
+        pass
+
 
 
 @triton.jit
@@ -278,11 +318,54 @@ class FlashAttentionTriton(torch.autograd.Function):
         raise NotImplementedError(
             "FlashAttention backward pass is not implemented yet."
         )
+    
 
+def test_backward():
+    is_casual = args.is_casual
+    context_len = 512
+    d_model = 64
+    batch_size = 64
+    q = torch.rand(batch_size, context_len, d_model).cuda() - 0.5
+    k = torch.rand(batch_size, context_len, d_model).cuda() - 0.5
+    v = torch.rand(batch_size, context_len, d_model).cuda() - 0.5
+    mask = torch.tril(torch.ones(context_len, context_len)).cuda()
+    mask = mask.to(torch.bool)
+    mask = rearrange(mask, "i j -> 1 i j")
+    q.requires_grad = True
+    k.requires_grad = True
+    v.requires_grad = True
+    from cs336_basics.model import scaled_dot_product_attention
+    out_bench = scaled_dot_product_attention(q, k, v, mask=mask if is_casual else None)
 
-if __name__ == "__main__":
+    dO = torch.rand_like(out_bench)
+    out_bench.backward(dO)
+    dq_bench, dk_bench, dv_bench = q.grad, k.grad, v.grad
+    q.grad = None
+    k.grad = None
+    v.grad = None
+
+    out = NaiveAttention.apply(q, k, v, is_casual)
+    assert torch.allclose(
+        out, out_bench, atol=1e-3
+    ), f"Max diff NaiveAttention: {(out - out_bench).abs().max():.6f}"
+    print("forward correctness test passed!")
+
+    out.backward(dO)
+    dq, dk, dv = q.grad, k.grad, v.grad
+    assert torch.allclose(
+        dq, dq_bench, atol=1e-3
+    ), f"Max diff dQ: {(dq - dq_bench).abs().max():.6f}"
+    assert torch.allclose(
+        dk, dk_bench, atol=1e-3
+    ), f"Max diff dK: {(dk - dk_bench).abs().max():.6f}"
+    assert torch.allclose(
+        dv, dv_bench, atol=1e-3
+    ), f"Max diff dV: {(dv - dv_bench).abs().max():.6f}"
+    print("backward correctness test passed!")
+
+def test_forward():
     torch.manual_seed(0)
-    is_casual = True
+    is_casual = args.is_casual
     context_len = 2048
     d_model = 64
     batch_size = 64
@@ -302,6 +385,7 @@ if __name__ == "__main__":
 
     out1 = FlashAttention.apply(q, k, v, is_casual)
     out2 = FlashAttentionTriton.apply(q, k, v, is_casual)
+    out3 = NaiveAttention.apply(q, k, v, is_casual)
 
     from cs336_basics.model import scaled_dot_product_attention
     out_bench = scaled_dot_product_attention(q, k, v, mask=mask if is_casual else None)
@@ -309,12 +393,17 @@ if __name__ == "__main__":
     assert torch.allclose(
         out1, out_bench, atol=1e-3
     ), f"Max diff FlashAttention: {(out1 - out_bench).abs().max():.6f}"
-    print("correctness test passed!")
+    print("FlashAttention correctness test passed!")
 
     assert torch.allclose(
         out2, out_bench, atol=1e-3
     ), f"Max diff FlashAttentionTriton: {(out2 - out_bench).abs().max():.6f}"
-    print("correctness test passed!")
+    print("FlashAttentionTriton correctness test passed!")
+
+    assert torch.allclose(
+        out3, out_bench, atol=1e-3
+    ), f"Max diff NaiveAttention: {(out3 - out_bench).abs().max():.6f}"
+    print("NaiveAttention correctness test passed!")
 
 
     mean_ms = triton.testing.do_bench(lambda: FlashAttentionTriton.apply(q, k, v, is_casual), quantiles=[0.5])
@@ -322,3 +411,16 @@ if __name__ == "__main__":
 
     mean_ms = triton.testing.do_bench(lambda: scaled_dot_product_attention(q, k, v, mask=mask if is_casual else None), quantiles=[0.5])
     print(f"PyTorch Attention: {mean_ms:.2f}ms")
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--test_forward", action="store_true")
+    parser.add_argument("-b", "--test_backward", action="store_true")
+    parser.add_argument("--is_casual", action="store_true")
+    args = parser.parse_args()
+    if args.test_forward:
+        test_forward()
+    if args.test_backward:
+        test_backward()
